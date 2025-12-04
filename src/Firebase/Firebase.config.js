4c3865425baac6a5ffa8171adb87d18d5b3917a9
md.rayhan <md.rayhanx617@gmail.com>
2025-12-04 10:11:07 +0600

    solve site error issue

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -7,15 +7,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 
 import { getAuth } from "firebase/auth";
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
